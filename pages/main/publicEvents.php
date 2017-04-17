6b8021f8859640c7fa2e9939a88ae5081c73de73
dalugoga <dalugoga@gmail.com>
2017-04-17 15:55:18 +0100

    public events page

--- pages/main/publicEvents.php
+++ pages/main/publicEvents.php
@@ -25,31 +25,24 @@
 	<div id="title" align="middle" style="margin-top:1%">
 		<h3> Public Events </h3>
 	</div>
+	
+	<?php
+		include_once('../../database/get_public_events.php');
+		$public_events = get_public_events();
 
+	?>
+	
+	<?php foreach($public_events as $public_event){?>
+	
 	<div id="pEvent" align= "left" class="all-75 small-100 tiny-100 push-center" >
 
-		<a href="#" class="large" >August Product Showcase </a>
-        <p class="small"> <i class="fa fa-calendar-o" aria-hidden="true"></i>  2/08/2017 17:30 <i class="fa fa-map-marker" aria-hidden="true"></i> Main Presentation Area</p>
+		<a href="#" class="large" > <? echo $public_event['name'] ?> </a>
+        <p class="small"> <i class="fa fa-calendar-o" aria-hidden="true"></i> <? echo $public_event['calendar_date'] ?> <? echo $public_event['calendar_time'] ?> <i class="fa fa-map-marker" aria-hidden="true"></i> <? echo $public_event['location'] ?> </p>
 		<hr>
-		<p> Showcase of all new products the company will release in the next months. Anyone can attend.</p>
-
-
+		<p> <? echo $public_event['description'] ?> </p>
 	</div>
-      <div id="pEvent" align= "left" class="all-75 small-100 tiny-100 push-center" >
+	
+	<?php } ?>
 
-		<a href="#" class="large" >27 Years Anniversary Celebration </a>
-          <p class="small"> <i class="fa fa-calendar-o" aria-hidden="true"></i> 17/12/2017 22:00 <i class="fa fa-map-marker" aria-hidden="true"></i> Canteen </p>
-		<hr>
-		<p> Our esteemed company is going to become one year older! Come celebrate with us!</p>
-
-
-	</div>
-      <div id="pEvent" align= "left" class="all-75 small-100 tiny-100 push-center" >
-
-		<a href="#" class="large" >Charity Event</a>
-        <p class="small"> <i class="fa fa-calendar-o" aria-hidden="true"></i> 24/02/2018 21:30 <i class="fa fa-map-marker" aria-hidden="true"></i> Canteen</p>
-		<hr>
-		<p>Event to raise funds to send to several organizations battling the ebola spread in Central Africa. </p>
-	</div>
 
 <?php include_once($BASE_DIR .'templates/common/footer.tpl'); ?>
