79163de028cfe23c080f2aeb77142ffdc3a7b5a4
Sara Santos <santos.sara96@gmail.com>
2017-04-22 16:35:22 +0100

    list events with Smarty

--- pages/admin/listEvents.php
+++ pages/admin/listEvents.php
@@ -3,15 +3,6 @@
   include_once($BASE_DIR .'database/events.php');
 
   $events = getAllEvents();
-  foreach ($events as $key => $value) { ?>
-    <tr>
-      <td >
-        <span class="ink-tooltip push-right" data-tip-text="Delete" data-tip-color="grey">
-          <i class="fa fa-times" aria-hidden="true" onclick="onClickDeleteEvent('<?=$value[idEvent]?>')"></i>
-        </span><br>
-        <a href="#"><?php echo $value['name'];?></a>
-        <h6 class="fw-300"><?echo $value['calendar_date']; echo " "; echo $value['calendar_time'];?></h6>
-        <p> <?php echo $value['description'];?> </p>
-      </td>
-    </tr>
-  <?php } ?>
+  $smarty->assign('events',$events);
+  $smarty->display('admin/list_events.tpl');
+ ?>
