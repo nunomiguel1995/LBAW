4fcc3bec4bcf47a2ea8fcbfcd748f0bf5163e1fb
dalugoga <dalugoga@gmail.com>
2017-04-22 19:55:15 +0100

    public events real smarty

--- pages/main/publicEvents.php
+++ pages/main/publicEvents.php
@@ -1,48 +1,8 @@
 <?php
-  include_once('../../config/init.php');
-  include_once($BASE_DIR .'templates/common/header.tpl');
+	include_once('../../config/init.php');
+	include_once('../../database/get_public_events.php');
+	$events = get_public_events();
+	
+	$smarty->assign('events', $events);
+	$smarty->display('main/public_events.tpl');
 ?>
-  <style type="text/css">
-      #pEvent{
-        background: white;
-        padding: 1%;
-        border-style: solid;
-        border-color: #dbdbdb;
-        border-radius: 15px;
-        margin-bottom: 2%;
-      }
-  </style>
-
-  <div id="top-image">
-    <figure class="ink-image bottom-space all-75 small-100 tiny-100 push-center">
-      <img src="../../images/events/public_events.jpg" class="imagequery">
-      <figcaption class="over-bottom">
-        Events for all the community
-      </figcaption>
-    </figure>
-  </div>
-
-	<div id="title" align="middle" style="margin-top:1%">
-		<h3> Public Events </h3>
-	</div>
-	
-	<?php
-		include_once('../../database/get_public_events.php');
-		$public_events = get_public_events();
-
-	?>
-	
-	<?php foreach($public_events as $public_event){?>
-	
-	<div id="pEvent" align= "left" class="all-75 small-100 tiny-100 push-center" >
-
-		<a href="#" class="large" > <? echo $public_event['name'] ?> </a>
-        <p class="small"> <i class="fa fa-calendar-o" aria-hidden="true"></i> <? echo $public_event['calendar_date'] ?> <? echo $public_event['calendar_time'] ?> <i class="fa fa-map-marker" aria-hidden="true"></i> <? echo $public_event['location'] ?> </p>
-		<hr>
-		<p> <? echo $public_event['description'] ?> </p>
-	</div>
-	
-	<?php } ?>
-
-
-<?php include_once($BASE_DIR .'templates/common/footer.tpl'); ?>
---
+++ templates/main/public_events.tpl
@@ -0,0 +1,36 @@
+
+{include file='common/header.tpl'}
+
+<style type="text/css">
+      #pEvent{
+        background: white;
+        padding: 1%;
+        border-style: solid;
+        border-color: #dbdbdb;
+        border-radius: 15px;
+        margin-bottom: 2%;
+      }
+  </style>
+
+  <div id="top-image">
+    <figure class="ink-image bottom-space all-75 small-100 tiny-100 push-center">
+      <img src="../../images/events/public_events.jpg" class="imagequery">
+      <figcaption class="over-bottom">
+        Events for all the community
+      </figcaption>
+    </figure>
+  </div>
+
+	<div id="title" align="middle" style="margin-top:1%">
+		<h3> Public Events </h3>
+	</div>
+	
+	{foreach $events as $event}
+		<div id="pEvent" align= "left" class="all-75 small-100 tiny-100 push-center" >
+			<a href="#" class="large" > {$event.name} </a>
+			<p class="small"> <i class="fa fa-calendar-o" aria-hidden="true"></i> {$event.calendar_date} {$event.calendar_time} <i class="fa fa-map-marker" aria-hidden="true"></i> {$event.location} </p>
+			<hr>
+			<p> {$event.description} </p>
+		</div>
+	{/foreach}
+{include file='common/footer.tpl'}
