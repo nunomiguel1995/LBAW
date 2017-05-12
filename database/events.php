8d7fb315e378cb1e1f379585c4d7af0860ab9ed7
dalugoga <dalugoga@gmail.com>
2017-05-12 15:35:16 +0100

    and another one

--- database/events.php
+++ database/events.php
@@ -144,5 +144,51 @@
 		$stmt->execute(array($idPost));
 		return $stmt->fetchAll();
 	}
+	
+	function getPostPoll($idPost){
+		global $conn;
+		$stmt = $conn->prepare('SELECT "idPoll", name
+								FROM poll 
+								INNER JOIN post
+								ON poll."idPost" = post."idPost"
+								WHERE post."idPost" = ?');
+		$stmt->execute(array($idPost));
+		return $stmt->fetchAll();
+	}
+	
+	function getPollOptions($idPoll){
+		global $conn;
+		$stmt = $conn->prepare('SELECT "idOption", "pollOption".name, votes
+								FROM poll 
+								INNER JOIN "pollOption"
+								ON poll."idPoll" = "pollOption"."idPoll"
+								WHERE poll."idPoll" = ?');
+		$stmt->execute(array($idPoll));
+		return $stmt->fetchAll();
+	}
+	
+	function isvoted($idPoll, $idUser, $idOption){
+		global $conn;
+		$stmt = $conn->prepare('SELECT *
+								FROM vote
+								INNER JOIN "pollOption"
+								ON "idOption" = "idPollOption"
+								INNER JOIN poll
+								ON "pollOption"."idPoll" = poll."idPoll"
+								WHERE "idUser" = ? 
+								AND poll."idPoll" = ?
+								AND "idOption" = ?');
+		$stmt->execute(array($idUser, $idPoll, $idOption));
+		return $stmt->fetchAll();
+	}
+	
+	function numberOfVotes($idOption){
+		global $conn;
+		$stmt = $conn->prepare('SELECT count("idUser")
+								FROM vote
+								WHERE "idPollOption" = ?');
+		$stmt->execute(array($idOption));
+		return $stmt->fetchAll();
+	}
 
 ?>
