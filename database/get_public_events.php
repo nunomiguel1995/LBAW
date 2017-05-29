d4717d7de6894bb8a0e800772f5158d33233ed42
Sara Santos <santos.sara96@gmail.com>
2017-05-29 10:52:16 +0100

    create event updated

--- database/get_public_events.php
+++ database/get_public_events.php
@@ -2,9 +2,10 @@
 
 function get_public_events() {
     global $conn;
-    $stmt = $conn->prepare('SELECT * 
+    $stmt = $conn->prepare('SELECT *
                             FROM event
 							WHERE "isPublic" = TRUE
+              AND calendar_date >= current_date
 							ORDER BY calendar_date ASC,
 							calendar_time ASC');
     $stmt->execute();
